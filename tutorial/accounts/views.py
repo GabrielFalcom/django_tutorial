055495ed19ce4ccfb794998102c41a4ac14886d2
Gabriel <gabrielnps@algartelecom.com.br>
2018-09-18 12:21:01 -0300

    '.'

--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import RegistrationForm
 # Create your views here.
 
+@login_required
 def home(request):
-    numbers = [1,2,3,4,5]
-    name = 'Gabriel'
-    args = {'name': name, 'numbers':numbers}
+    args = {'current_user': request.user}
+
     return render(request, 'accounts/home.html',args)
 
-    # return render(request, 'accounts/login.html',args)
+def register(request):
+    if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts')
+    else:
+        # form = UserCreationForm()
+        form = RegistrationForm()
 
-    # return HttpResponse('<em>Home Page!</em>')
+    args = {'form': form}
+    return render(request, 'accounts/reg_form.html', args)
